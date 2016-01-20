5d3038279c757e34d0be8f780d017f6f2bde3841
Oleh Kravchenko <oleg@kaa.org.ua>
2016-01-20 17:37:55 +0200

    [lt-1] Fix Doxygen errors

---
+++ include/doxygen.h
@@ -0,0 +1,17 @@
+/**
+ * @file
+ */
+
+#ifdef __DOXYGEN
+#error this file only for doxygen
+
+/** I/O error */
+#define EIO 5
+
+/** Broken pipe */
+#define EPIPE 32
+
+/** Last error number */
+int errno;
+
+#endif /* __DOXYGEN */
