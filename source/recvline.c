7b07089c7dd1f9d663da85dd94b70ead733df322
Oleh Kravchenko <oleg@kaa.org.ua>
2016-01-20 17:34:49 +0200

    [lt-1] Fix cppcheck warning for recvline()

--- source/recvline.c
+++ source/recvline.c
@@ -13,23 +13,23 @@ ssize_t recvline(int fd, char *buf, size_t size, const char *d, size_t dlen)
 	size_t eolindex = 0;
 	size_t received = 0;
 
-	char c;
-	ssize_t rc;
-
 	while(received < size && eolindex < dlen) {
-		if ((rc = recv(fd, &c, sizeof(c), 0)) <= 0) {
+		char sym;
+
+		ssize_t rc = recv(fd, &sym, sizeof(sym), 0);
+		if (0 >= rc) {
 			/* connection closed? error? */
 			return (rc);
 		}
 
 		/* increment or reset delimiter match */
-		if (d[eolindex] == c) {
+		if (d[eolindex] == sym) {
 			++ eolindex;
 		} else {
 			eolindex = 0;
 		}
 
-		buf[received ++] = c;
+		buf[received ++] = sym;
 	}
 
 	return (received);
